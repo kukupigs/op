4e502c164a6b81c3011b0a1346861a880f854e37
kukupigs <kukupigs@sina.com>
2020-12-24 05:58:06 +0800

    Update diy-part1.sh

--- router/linksys_wrt32x/diy-part1.sh
+++ router/linksys_wrt32x/diy-part1.sh
@@ -10,9 +10,6 @@
 # Uncomment a feed source
 sed -i 's/#src-git helloworld/src-git helloworld/g' ./feeds.conf.default
 # sed -i 's/\"#src-git\"/\"src-git\"/g' feeds.conf.default
-sed -i 's/#src-git kenzo https://github.com/kenzok8/openwrt-packages' ./feeds.conf.default
-./scripts/feeds update -a
-./scripts/feeds install -a
 # Add a feed source
-# sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
-
+ sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' ./feeds.conf.default
